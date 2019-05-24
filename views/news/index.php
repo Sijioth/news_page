ceeb548dcdb311b8505cc642deb97c946c9557c8
Sijioth <jhnsmthmit@gmail.com>
2019-05-24 12:37:02 +0300

    feat: Add style to contacts in footer.

--- views/news/index.php
+++ views/news/index.php
@@ -102,45 +102,49 @@
             </div>
         </section>
     </main>
-    <footer class="wrapper">
-        <ul class="nav">
-            <li>
-                <a href="#">Privacy</a>
-            </li>
-            <li>
-                <a href="#">Search</a>
-            </li>
-            <li>
-                <a href="#">Glossary</a>
-            </li>
-            <li>
-                <a href="#">?Arbolegal?</a>
-            </li>
-            <li>
-                <a href="#">Contacts</a>
-            </li>
-        </ul>
-        <p class="contacts">
-            <img src="../template/images/impacto-logo.png" alt="Webimpacto" class="logo-small">
-            <ul class="address">
-                <li class="">Calle Ciudad Real 2</li>
-                <li class="cumber-bread noselect"></li>
-                <li class="">Yuncos Valley</li>
-                <li class="cumber-bread noselect"></li>
-                <li class="">Toledo, Spain</li>
-                <li class="cumber-bread noselect"></li>
-                <li class="tel"><a href="tel:+34 900 900 900">Tel +34 900 900 900</a></li>
-                <li class="cumber-bread noselect"></li>
-                <li class="fax"><a href="fax:+34 900900900">Fax +34 900900900</a></li>
+    <footer>
+        <section class="wrapper">
+            <ul class="nav">
+                <li>
+                    <a href="#">Privacy</a>
+                </li>
+                <li>
+                    <a href="#">Search</a>
+                </li>
+                <li>
+                    <a href="#">Glossary</a>
+                </li>
+                <li>
+                    <a href="#">?Arbolegal?</a>
+                </li>
+                <li>
+                    <a href="#">Contacts</a>
+                </li>
             </ul>
-            <a class="mailto" href="mailto:info@webimpacto.com">info@webimpacto.com</a>
-        </p>
-        <p class="copyright">
-            Webimpacto S.L. <span>&#8226;</span> News
-            <a href="https://webimpacto.agency" target="_blank" rel="noopener noreferrer">Powered by impacto
-                <img src="../template/images/logo-img.png" alt="&#174;">
-            </a>
-        </p>
+            <div class="contacts">
+                <img src="../template/images/impacto-logo.png" alt="Webimpacto" class="logo-small">
+                <ul class="address">
+                    <li class="">Calle Ciudad Real 2</li>
+                    <li class="cumber-bread noselect">|</li>
+                    <li class="">Yuncos Valley</li>
+                    <li class="cumber-bread noselect">|</li>
+                    <li class="">Toledo, Spain</li>
+                    <li class="cumber-bread noselect">|</li>
+                    <li class="tel"><a href="tel:+34 900 900 900">Tel +34 900 900 900</a></li>
+                    <li class="cumber-bread noselect">|</li>
+                    <li class="fax"><a href="fax:+34 900900900">Fax +34 900900900</a></li>
+                </ul>
+                <a class="mailto" href="mailto:info@webimpacto.com">info@webimpacto.com</a>
+            </div>
+        </section>
+        <section class="copyright">
+            <div class="copyright__content wrapper">
+                Webimpacto S.L. <span>&#8226;</span> News
+                <a href="https://webimpacto.agency" target="_blank" rel="noopener noreferrer">Powered by impacto
+                    <img src="../template/images/logo-img.png" alt="&#174;">
+                </a>
+            </div>
+        </section>
     </footer>
 </body>
 </html>
