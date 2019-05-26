61a51e4031b5acafccf9dbe2d903c28c4c42aa15
Sijioth <jhnsmthmit@gmail.com>
2019-05-26 20:06:51 +0300

    feat: Change autoload to modern version.

--- components/Autoload.php
+++ components/Autoload.php
@@ -1,18 +1,5 @@
 <?php
-
-spl_autoload_register('autoloader');
-
-function autoloader($className) {
-    # list all the class directories in the array
-    $array_paths = array(
-        '/models',
-        '/components',
-    );
-
-    foreach($array_paths as $path) {
-        $path = ROOT . $path . $className . '.php';
-        if (is_file($path)) {
-            include_once($path);
-        }
+    function autoloader($pClassName) {
+        include(__DIR__ . "/" . $pClassName . ".php");
     }
-}
+    spl_autoload_register("autoloader");
